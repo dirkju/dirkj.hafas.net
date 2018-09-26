5f272f6ad8d0b337ecfed72a4279728cd15bce37
Dirk Junghanns <dirkju@outlook.com>
2018-09-26 22:56:53 +0200

    added Vasttrafik endpoint

--- dirkj.hafas.net/HafasEndpoint.cs
+++ dirkj.hafas.net/HafasEndpoint.cs
@@ -1,14 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace dirkj.hafas.net
+﻿namespace dirkj.hafas.net
 {
     public class HafasEndpoint
     {
-        public string BaseUrl;
-        public string ApiVersion;
-        public string DateFormat;
-        public string TimeFormat;
+        public string BaseUrl { get; set; }
+        public string TokenUrl { get; set; }
+        public string ApiVersion { get; set; }
+        public string DateFormat { get; set; }
+        public string TimeFormat { get; set; }
+        public string ApiKey { get; set; }
     }
 }
--- dirkj.hafas.net/HafasEndpoints.cs
+++ dirkj.hafas.net/HafasEndpoints.cs
@@ -1,14 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace dirkj.hafas.net
+﻿namespace dirkj.hafas.net
 {
     public static class HafasEndpoints
     {
-        public static HafasEndpoint DSB = new HafasEndpoint{
+        public static HafasEndpoint DSB = new HafasEndpoint
+        {
             BaseUrl = "http://xmlopen.rejseplanen.dk/bin/rest.exe/",
             DateFormat = "dd.MM.yyyy",
+            TimeFormat = "HH:mm"
+        };
+
+        public static HafasEndpoint Vasttrafik = new HafasEndpoint
+        {
+            BaseUrl = "https://api.vasttrafik.se/bin/rest.exe/v2",
+            TokenUrl = "https://api.vasttrafik.se/token",
+            DateFormat = "yyyy-MM-dd",
             TimeFormat = "HH:mm"
         };
     }
